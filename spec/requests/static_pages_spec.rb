7193c64b0be82df04f72cf95f1691e9a2d141c89
nphollon <nhollon@thoughtworks.com>
2013-02-15 11:12:06 -0600

    removed custom page title from home page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,52 +4,56 @@ describe "Static Pages" do
   let(:title_prefix) {"Ruby on Rails Tutorial Sample App | "}
 
   describe "Home Page" do
+    before {visit '/static_pages/home'}
+
   	it "should have the content 'Sample App'" do
-  		visit '/static_pages/home'
   		page.should have_selector('h1', text: 'Sample App')
   	end
 
-  	it "should have the title 'Home'" do
-			visit '/static_pages/home'
+  	it "should have the base title" do
 			page.should have_selector('title',
-				text: title_prefix + "Home")
+				text: "Ruby on Rails Tutorial Sample App")
 	  end
+
+    it "should not have a custom page title" do
+      page.should_not have_selector('title', text: ' | ')
+    end
   end
 
   describe "Help Page" do
+    before {visit '/static_pages/help'}
+
   	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
   		page.should have_selector('h1', text: 'Help')
   	end
 
   	it "should have the title 'Help'" do
-			visit '/static_pages/help'
 			page.should have_selector('title',
 				text: title_prefix + "Help")
 	  end
   end
 
   describe "About Page" do 
+    before {visit '/static_pages/about'}
+
   	it "should have the content 'About Us'" do
-  		visit '/static_pages/about'
   		page.should have_selector('h1', text: 'About Us')
   	end
 
   	it "should have the title 'About'" do
-			visit '/static_pages/about'
 			page.should have_selector('title',
 				text: title_prefix + "About Us")
 	  end
   end
 
   describe "Contact Page" do
+    before {visit '/static_pages/contact'}
+
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
       page.should have_selector('h1', text: 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
       page.should have_selector('title',
         text: title_prefix + "Contact")
     end
