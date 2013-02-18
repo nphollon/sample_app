793a9e40b890ad034aba96b4e2538f41b685dbec
nphollon <nhollon@thoughtworks.com>
2013-02-18 10:15:10 -0600

    authenticate user with has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,15 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
   	uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  has_secure_password
 
   before_save { |user| user.email = email.downcase }
 end
