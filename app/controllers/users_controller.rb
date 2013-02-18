e85f5b60842c0bbe1d2dcdc043aea125a990db89
nphollon <nhollon@thoughtworks.com>
2013-02-18 12:13:02 -0600

    Handle signup success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      # Handle successful save
+      redirect_to user_path(@user)
     else
       render 'new'
     end
