d25d6da090f06ac27569431cd27b4539578e4d68
nphollon <nhollon@thoughtworks.com>
2013-02-18 15:36:55 -0600

    Add remember_token to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,10 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { email.downcase! }
+  before_save :create_remember_token
+
+  private
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
