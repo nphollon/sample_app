034a803a3fc5e1452da5988d2ce3ee69173e9385
nphollon <nhollon@thoughtworks.com>
2013-02-25 10:56:41 -0600

    Order microposts by created_at; Destroy microposts associated with deleted users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -138,4 +138,23 @@ describe User do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+
+  describe "micropost associations" do
+    before { @user.save }
+    let!(:older_micropost) { FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago) }
+    let!(:newer_micropost) { FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago) }
+
+    it "should have the microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.dup
+      @user.destroy
+      microposts.should_not be_empty
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+  end
 end
