0d3f824e5a35369337bc3925a8f635b29d8c3c35
nphollon <nhollon@thoughtworks.com>
2013-02-25 09:51:11 -0600

    validate presence of user id

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Micropost do
@@ -10,4 +21,11 @@ describe Micropost do
 
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
+
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+    before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
 end
