e4660f3b180cdb2a945b0d3438fa58ee04a98b29
nphollon <nhollon@thoughtworks.com>
2013-02-25 10:03:59 -0600

    relate microposts and users with belongs_to and has_many

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -10,7 +10,8 @@
 #
 
 class Micropost < ActiveRecord::Base
-  attr_accessible :content, :user_id
+  attr_accessible :content
+  belongs_to :user
 
   validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,14 +14,14 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+  has_many :microposts
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
   	uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-
-  has_secure_password
 
   before_save { email.downcase! }
   before_save :create_remember_token
